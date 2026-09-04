110cb86bde16b7cd2a08c020e2d9f88bdbebabdf
benjamin <benjaminyakobi@gmail.com>
2026-09-04 10:46:19 +0300

    ref:feat: new handleEvent dispacter func to handle three types of tcell events - keyboard, mouse & resize

--- internal/game/input.go
+++ internal/game/input.go
@@ -1,3 +1,26 @@
 package game
 
 // Contains game input events code
+
+import (
+	"github.com/gdamore/tcell/v3"
+)
+
+// NOTE: event dispatcher
+func handleEvent(
+	renderer *renderer,
+	engine *engine,
+	state *loopState,
+	ev tcell.Event,
+) {
+	// switch ev := ev.(type) {
+	// case *tcell.EventResize:
+	// 	handleResize(renderer, engine, state)
+	//
+	// case *tcell.EventKey:
+	// 	handleKey(renderer, engine, state, ev)
+	//
+	// case *tcell.EventMouse:
+	// 	handleMouse(renderer, engine, state, ev)
+	// }
+}
