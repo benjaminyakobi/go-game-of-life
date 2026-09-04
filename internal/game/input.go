968196faca00f4d4ed7479b1cd599a7f611d4b93
benjamin <benjaminyakobi@gmail.com>
2026-09-04 14:12:59 +0300

    ref: added new event dispatcher for keyboard (inner functions) not implemented yet

--- internal/game/input.go
+++ internal/game/input.go
@@ -3,6 +3,8 @@ package game
 // Contains game input events code
 
 import (
+	"time"
+
 	"github.com/gdamore/tcell/v3"
 )
 
@@ -12,17 +14,19 @@ func handleEvent(
 	engine *engine,
 	state *loopState,
 	ev tcell.Event,
-) {
+) bool {
 	// switch ev := ev.(type) {
 	// case *tcell.EventResize:
 	// 	handleResize(renderer, engine, state)
 	//
 	// case *tcell.EventKey:
-	// 	handleKey(renderer, engine, state, ev)
+	// 	return handleKey(renderer, engine, state, ev)
 	//
 	// case *tcell.EventMouse:
 	// 	handleMouse(renderer, engine, state, ev)
 	// }
+
+	return false
 }
 
 func handleResize(
@@ -39,4 +43,48 @@ func handleResize(
 	}
 
 	renderer.screen.Show()
+}
+
+// NOTE: event dispatcher for keyboard events
+func handleKey(
+	renderer *renderer,
+	engine *engine,
+	state *loopState,
+	ev *tcell.EventKey,
+) bool {
+	keyNow := time.Now()
+
+	// Escape
+	if ev.Key() == tcell.KeyEscape {
+		// We need to preserve the original return behavior.
+		return true
+	}
+
+	// Pause
+	// handlePause(renderer, engine, state, ev)
+
+	// Previous generation
+	// handlePreviousGeneration(renderer, engine, state, ev)
+
+	// Next generation
+	// handleNextGeneration(renderer, engine, state, ev)
+
+	// Choose predefined pattern
+	// handlePredefinedPattern(renderer, engine, state, ev)
+
+	// Run / resume / accept predefined pattern
+	// handleRun(renderer, engine, state, ev)
+
+	// Increase speed
+	// handleIncreaseSpeed(renderer, state, ev, keyNow)
+
+	// Decrease speed
+	// handleDecreaseSpeed(renderer, state, ev, keyNow)
+
+	// Stop
+	// handleStop(renderer, engine, state, ev)
+
+	state.lastKeyTime = keyNow
+
+	return false
 }
