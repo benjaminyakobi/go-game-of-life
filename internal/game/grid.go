192092fe21501a19c10526eed479e0a949385a07
benjamin <benjaminyakobi@gmail.com>
2026-09-10 19:51:24 +0300

    fix: moved the if statement that checks the grid boundaries to the func that draws the specific living cell

--- internal/game/grid.go
+++ internal/game/grid.go
@@ -155,14 +155,14 @@ func (r *renderer) drawSingleDeadCellOnGrid(c cell) {
 }
 
 func (r *renderer) drawSingleLivingCellOnGrid(c cell) {
-	r.screen.Put(c.PosX, c.PosY, "@", css.greenYellow)
+	if c.PosY > r.gridOffset && c.PosY < r.gridHeight-1 && c.PosX > 0 && c.PosX < r.gridWidth-1 {
+		r.screen.Put(c.PosX, c.PosY, "@", css.greenYellow)
+	}
 }
 
 func (r *renderer) drawLivingCellsOnGrid(cs cellsSet) {
 	for c := range cs {
-		if c.PosY > r.gridOffset && c.PosY < r.gridHeight-1 && c.PosX > 0 && c.PosX < r.gridWidth-1 {
-			r.drawSingleLivingCellOnGrid(c)
-		}
+		r.drawSingleLivingCellOnGrid(c)
 	}
 }
 
